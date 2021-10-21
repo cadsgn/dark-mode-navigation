1198d6d8b4318cbfba4f1a5742c40bd224bb1c8b
cadsgn <krlos.ad@gmail.com>
2021-10-21 22:37:52 +0200

    Start styled theme

--- src/components/layout/head.tsx
+++ src/components/layout/head.tsx
@@ -1,21 +1,17 @@
 import React from "react";
-import { Title, Meta } from "react-head";
-import Montserrat300Woff2 from "../../../static/fonts/montserrat/montserrat-300.woff2";
-import Montserrat400Woff2 from "../../../static/fonts/montserrat/montserrat-400.woff2";
-import Montserrat600Woff2 from "../../../static/fonts/montserrat/montserrat-600.woff2";
-import OrelegaOne400Woff2 from "../../../static/fonts/orelega-one/orelega-one-400.woff2";
+import { Helmet } from "react-helmet";
 
 const Head = () => (
-  <>
-    <Title>Carlos Alvarez | UX/UI Developer</Title>
-    <Meta
+  <Helmet>
+    <title>Carlos Alvarez | UX/UI Developer</title>
+    <meta
       name='description'
       content='Carlos Alvarez design web page. Ux Ui and frontend developer'
     />
-    <Meta name='author' content='Carlos Alvarez Design' />
-    {/* <Meta name='robots' content={getRobots()} /> */}
-    {/* <Meta name='theme-color' content='#d2312e' /> */}
-  </>
+    <meta name='author' content='Carlos Alvarez Design' />
+    {/* <meta name='robots' content={getRobots()} /> */}
+    {/* <meta name='theme-color' content='#d2312e' /> */}
+  </Helmet>
 );
 
 export default Head;
