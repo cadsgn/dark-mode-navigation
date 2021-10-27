c5c2e524d12219a62a63077d518b6d7193dac6d4
cadsgn <krlos.ad@gmail.com>
2021-10-27 13:34:53 +0200

    adding gzip

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,12 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "webapp",
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet"],
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet",
+    {
+      resolve: 'gatsby-plugin-zopfli',
+      options: {
+        extensions: ['css', 'html', 'js', 'svg']
+      }
+    }
+  ],
 };
